6a8fcc7c112a66b031858b08c04c25f9a6155e1b
Joseph Schilz <joseph@schilz.org>
2015-09-08 08:39:10 -0700

    Pass tests.

--- tests/CSRFTest.php
+++ tests/CSRFTest.php
@@ -18,13 +18,13 @@ HTML;
 $expected = <<<HTML
 <html>
     <head>
-<script>var CSRFTOKEN = '2e658f027995c797e13f623f3308cf9c';</script>
+<script>var CSRFTOKEN = '{{ token }}';</script>
 
 
     </head>
     <body>
         <form>
-<input type=hidden name=csrf_token value=2e658f027995c797e13f623f3308cf9c>
+<input type=hidden name=csrf_token value={{ token }}>
 
 
         </form>
@@ -35,14 +35,34 @@ HTML;
 class CSRFTest extends PHPUnit_Framework_TestCase {
 
     public function testInsertsCSRF() {
-        throw new \Exception("Test not implemented.");
+        global $in, $expected;
+
+        // Test no CSRF token required, none provided
+        $_SERVER['REQUEST_METHOD'] = "GET";
+
+        ob_start();
+
+        CSRF::init();
+        echo $in;
+        ob_end_flush();
+
+        $result = ob_get_clean();
+
+        $newExpected = str_replace("{{ token }}", $_SESSION['csrf_token'], $expected);
+
+        $result = str_replace("\r\n", "\n", $result);
+        $newExpected = str_replace("\r\n", "\n", $newExpected);
+
+        // If we get here without an exception, then the test has passed
+        $this->assertEquals($newExpected, $result);
     }
 
     public function testCheckCSRFNoTokenRequiredNoTokenProvided() {
         // Test no CSRF token required, none provided
         $_SERVER['REQUEST_METHOD'] = "GET";
+
         CSRF::init();
-        ob_end_flush();
+        ob_get_clean();
 
         // If we get here without an exception, then the test has passed
         $this->assertTrue(True);
@@ -54,8 +74,9 @@ class CSRFTest extends PHPUnit_Framework_TestCase {
     public function testCheckCSRFTokenRequiredNoTokenProvided() {
         // Test CSRF token required, but none provided
         $_SERVER['REQUEST_METHOD'] = "POST";
+
         CSRF::init();
-        ob_end_clean();
+        ob_get_clean();
     }
 
     public function testCheckCSRFTokenRequiredCorrectTokenProvided() {
@@ -69,8 +90,7 @@ class CSRFTest extends PHPUnit_Framework_TestCase {
         $_POST['csrf_token'] = $_SESSION['csrf_token'];
 
         CSRF::init();
-
-        ob_end_clean();
+        ob_get_clean();
 
         // If we got here without an exception, then the test has passed
         $this->assertTrue(True);
@@ -90,11 +110,6 @@ class CSRFTest extends PHPUnit_Framework_TestCase {
         $_POST['csrf_token'] = "incorecttoken";
 
         CSRF::init();
-
-        ob_end_clean();
-
-        // If we got here without an exception, then the test has passed
-        $this->assertTrue(True);
     }
 
 
@@ -104,9 +119,7 @@ class CSRFTest extends PHPUnit_Framework_TestCase {
         $this->assertFalse(isset($_SESSION['csrf_token']));
 
         CSRF::init();
-
         $this->assertTrue(isset($_SESSION['csrf_token']));
-
-        ob_end_clean();
+        ob_get_clean();
     }
 }
